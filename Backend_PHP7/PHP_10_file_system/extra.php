5b7b9bedc0b8bfc645f31924cd9ab7ef9911dac6
tehno-for <keoroot@gmail.com>
2022-01-08 01:25:05 +0400

    pollindrome

--- Backend_PHP7/PHP_10_file_system/extra.php
+++ Backend_PHP7/PHP_10_file_system/extra.php
@@ -1,4 +1,22 @@
+<?php
 echo substr('vtpds', -3, 1).'<br>'; //p
-echo substr('vtpdsmt', 2, 2); //pd
+echo substr('vtpdsmt', 2, 2).'<br>'; //pd
 
+//---------------------------- polindrome -----------------
 
+function isPalindrome($x) {
+    $m = 0;
+    switch($x){
+        case $x<=11:
+        echo $x . " is not a polindrome!";
+        exit;
+        default:
+        if(strlen($x)% 2 == 0){
+            
+        }
+        echo $x . " is a polindrome!";
+
+    }
+     
+}
+isPalindrome(121);
