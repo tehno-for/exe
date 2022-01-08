b41295cd754df4b0cd77fced499b810b8515cb97
tehno-for <keoroot@gmail.com>
2022-01-08 14:30:30 +0400

    leetcode

--- Backend_PHP7/PHP_10_file_system/extra.php
+++ Backend_PHP7/PHP_10_file_system/extra.php
@@ -3,6 +3,15 @@ echo substr('vtpds', -3, 1).'<br>'; //p
 echo substr('vtpdsmt', 2, 2).'<br>'; //pd
 
 //---------------------------- polindrome -----------------
+function isPol($x) {
+    $d = strlen($x)-strlen($x)% 2;
+    for($i=0; $i<$d; $i++)
+        if(substr($x, $i, 1) !== substr($x, gmp_neg($i+1), 1)){
+            echo $x . " is not a polindrome!";
+        }
+    }
+}
+
 
 function isPalindrome($x) {
     $m = 0;
@@ -11,6 +20,7 @@ function isPalindrome($x) {
         echo $x . " is not a polindrome!";
         exit;
         default:
+        function 
         if(strlen($x)% 2 == 0){
             
         }
