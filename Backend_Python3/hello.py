75901e850c5a8bf61eeb701136f1d633d28b98a8
tehno-for <keoroot@gmail.com>
2022-02-11 21:30:38 +0400

    Python new

--- Backend_Python3/hello.py
+++ Backend_Python3/hello.py
@@ -1,2 +1,8 @@
 first_name = input("What is you name?")
+if first_name == "Craig":
+    print("Good desigion learn Python {}".format(first_name))
+elif first_name == 'Max':
+    print("Everyone include {} learn Python".format(first_name))
+else:
+    print("You should to learn Python {}".format(first_name))
 print("How are you", first_name)
