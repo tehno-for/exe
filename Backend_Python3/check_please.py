afddb4a5d7e01aaad4e62a486c58b4e2d06549bd
tehno-for <keoroot@gmail.com>
2022-02-18 22:19:04 +0400

    Python new3

--- Backend_Python3/check_please.py
+++ Backend_Python3/check_please.py
@@ -1,4 +1,10 @@
+import math
+
 def split_check(total, number_of_people):
-    cost_per_person = total / number_of_people
-    return cost_per_person
-print(split_check(40, 8))
+     return math.ceil(total / number_of_people)
+# print(split_check(40.45, 8))
+
+total = float(input("What is total? "))
+number_of_people = int(input("How many people? "))
+amount_due = split_check(total, number_of_people)
+print("Each person owes ${}".format(amount_due))
